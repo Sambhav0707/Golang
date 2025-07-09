7d86985fe423cb4e11c757e1c53c68610afd4116
sambhav dalal <sambhavdalal1@gmail.com>
2025-07-09 12:42:21 +0530

    data types in Go

---
+++ data_types.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func print(){
+
+	var favNum int = 7 // in go writing the variable starts with keyword var and then the variable name then its type and then its value
+    var favName string = "confidential info"
+	var negNum int16 = 1600
+	var isgreat bool = true
+	fmt.Println(favNum , favName , negNum , isgreat)
+
+}
--- hello_sambhav.go
+++ hello_sambhav.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Sambhav 07!!!!")
+	print()
 }
