2a9c74689d92c96785e1341fb3c81783e9f77ffc
sambhav dalal <sambhavdalal1@gmail.com>
2025-08-05 10:47:30 +0530

    changeas

--- funtions/functions.go
+++ funtions/functions.go
@@ -2,17 +2,15 @@ package main
 
 import "fmt"
 
+func addLoveNumbers(a int, b int) int {
 
-
-func addLoveNumbers(a int , b int ) int {
-	
 	fmt.Println(a + b)
 	return a + b
 }
 
-// function with return name 
+// function with return name
 
-func addLoveNumbersWithReturn(a int , b int ) (result int ){
+func addLoveNumbersWithReturn(a int, b int) (result int) {
 	result = a + b
 	fmt.Println(result)
 	return
@@ -20,36 +18,32 @@ func addLoveNumbersWithReturn(a int , b int ) (result int ){
 
 // function can return multiple values
 
-func addLoveNumbersWithMultipleReturn(a int , b int ) (result int , result2 int ){
+func addLoveNumbersWithMultipleReturn(a int, b int) (result int, result2 int) {
 	result = a + b
 	result2 = a - b
-	fmt.Println(result,result2)
+	fmt.Println(result, result2)
 	return
 }
 
-
 // recursive function
-func recursiveFunction(a int) int{
-	if a == 0{
-		return 0
+func recursiveFunction(a int) int {
+	if a == 0 {
+		return 7
 	}
 	fmt.Println(a)
-	recursiveFunction(a-1)
+	recursiveFunction(a - 1)
 	return a
 
 }
 
-
 func main() {
- var a int  = 7
- var b int = 27
+	var a int = 7
+	var b int = 27
 
-
- addLoveNumbers(a,b)
- addLoveNumbersWithReturn(a,b)
- _ , result2 :=addLoveNumbersWithMultipleReturn(27 , 7)
- fmt.Println(result2)
- recursiveFunction(0)
-
+	addLoveNumbers(a, b)
+	addLoveNumbersWithReturn(a, b)
+	_, result2 := addLoveNumbersWithMultipleReturn(27, 7)
+	fmt.Println(result2)
+	recursiveFunction(0)
 
 }
